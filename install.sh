ac9b1fd9833c5a2ec0a9a0ffea4c5fb5545ef943
Daniil <32302524+Bazhyk@users.noreply.github.com>
2021-04-12 18:43:14 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+install.sh
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo apt-get install python3-pip -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker ubuntu
+exit
